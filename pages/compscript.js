0f153301394e33c0633f876c214b6238deedb31b
Niketan Ameta <129369223+DammNIKX@users.noreply.github.com>
2024-08-13 11:23:34 +0530

    Add files via upload

---
+++ pages/compscript.js
@@ -0,0 +1,6 @@
+document.getElementById('photoForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Add your form submission logic here
+    // You may want to use AJAX to send the form data to the server
+  });
+  
